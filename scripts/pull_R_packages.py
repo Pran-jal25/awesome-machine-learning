0d046a0b5273923a644f800ccd065eef07d60493
Pran-jal25 <pranjalchoubeyxc@gmail.com>
2026-06-24 18:10:05 +0530

    fix: use context manager to ensure file handle is properly closed

--- scripts/pull_R_packages.py
+++ scripts/pull_R_packages.py
@@ -11,20 +11,20 @@ import urllib.request
 import codecs
 import random
 
-text_file = codecs.open("Packages.txt", encoding='utf-8', mode="w")
-d = pq(url='http://cran.r-project.org/web/views/MachineLearning.html',
-       opener=lambda url, **kw: urllib.request.urlopen(url).read())
+with codecs.open("Packages.txt", encoding='utf-8', mode="w") as text_file:
+    d = pq(url='http://cran.r-project.org/web/views/MachineLearning.html',
+           opener=lambda url, **kw: urllib.request.urlopen(url).read())
 
-for e in d("li").items():
-    package_name = e("a").html()
-    package_link = e("a")[0].attrib['href']
-    if '..' in package_link:
-        package_link = package_link.replace("..",
-                                            'http://cran.r-project.org/web')
-        dd = pq(url=package_link, opener=lambda url,
-                **kw: urllib.request.urlopen(url).read())
-        package_description = dd("h2").html()
-        text_file.write(" [%s](%s) - %s \n" % (package_name, package_link,
-                                               package_description))
-        # print("* [%s](%s) - %s" % (package_name,package_link,
-        #                            package_description))
+    for e in d("li").items():
+        package_name = e("a").html()
+        package_link = e("a")[0].attrib['href']
+        if '..' in package_link:
+            package_link = package_link.replace("..",
+                                                'http://cran.r-project.org/web')
+            dd = pq(url=package_link, opener=lambda url,
+                    **kw: urllib.request.urlopen(url).read())
+            package_description = dd("h2").html()
+            text_file.write(" [%s](%s) - %s \n" % (package_name, package_link,
+                                                   package_description))
+            # print("* [%s](%s) - %s" % (package_name,package_link,
+            #                            package_description))
